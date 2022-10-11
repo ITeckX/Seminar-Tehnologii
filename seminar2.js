009fdf136f2b5cd3c2f937ce80b4e8ce37570dfd
ITeckX <turcanucalin2001@gmail.com>
2022-10-11 17:37:52 +0300

    seminar 2

--- seminar2.js
+++ seminar2.js
@@ -61,8 +61,8 @@ function checkDivisible (n, divisor) {
     }
 }
 
-console.log(checkDivisible(10, 2))
-console.log(checkDivisible(10, 3))
+console.log('Check divisible 10, 2 '+checkDivisible(10, 2))
+console.log('Check divisible 10, 3 '+checkDivisible(10, 3))
 
 function occurences(text, character){
     return text.split(character).length - 1;
@@ -71,9 +71,73 @@ function occurences(text, character){
 console.log(occurences("sample text", "e"));
 
 var evenBois=[];
-function even (input) {
-for(var i=0;i<input.length;i++) if(input[i]%2==0)evenBois.push(input[i])
+function even() {
+    let args = arguments;
+    for (var i = 0; i < args[0].length; i++)
+        if (args[0][i] % 2 == 0)
+            evenBois.push(args[0][i])
 }
 
 even([2,4,6,7,8])
 console.log(evenBois);
+evenBois = [];
+even([2, 5, 6, 7]);
+console.log(evenBois);
+evenBois = [];
+even([2, 5, 6, 7,9,10]);
+console.log(evenBois);
+evenBois = [];
+even([2]);
+console.log(evenBois);
+
+
+//console.log(3 == '3');
+//console.log(3 === '3');
+
+/*console.log(true + true + true == 3);*/
+const checkPrime = (n) => {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (!(n % i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+if (process.argv.lengh < 3) {
+    console.log('not enough params')
+} else {
+    console.log(checkPrime(parseInt(process.argv[2])))
+}
+
+var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+
+for (var day in days) {
+    console.log(day);
+    console.log(days[day]);
+}
+console.log()
+for (var day of days) {
+    console.log(day);
+}
+
+function getFrequency() {
+    let args = arguments;
+    let words = [];
+    let frequency = [];
+    let text = args[0].split(' ');
+    for (var word in text) {
+        if (words.includes(text[word])==false) {
+            words.push(text[word]);
+            frequency[words.indexOf(text[word])] = 1;
+        }
+        else {
+            frequency[words.indexOf(text[word])]++;
+        }
+    }
+    for (var i = 0; i < words.length; i++) {
+        console.log(words[i] + ': ' + frequency[i]);
+    }
+}
+var temp = "the quick brown fox jumps over the lazy dog";
+getFrequency("the quick brown fox jumps over the lazy dog");
